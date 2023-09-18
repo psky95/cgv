dd63c4311c9d55dedc12c2819a37927a4dd61060
psky95 <142555226+psky95@users.noreply.github.com>
2023-09-18 13:58:29 +0900

    Add files via upload

---
+++ movieaction/script.js
@@ -0,0 +1,22 @@
+// get video element id
+var vidClip = document.getElementById("myVideo"); 
+
+// play video event
+function playVid() { 
+    myVideo.play();
+} 
+
+// pause video event
+function pauseVid() { 
+  myVideo.pause(); 
+}
+
+// toggle button class on click
+$('button').on('click', function() {
+  $('.first, .second').toggle();
+});
+
+// toggle button class when finished
+vidClip.onended = function(e) {
+  $('.first, .second').toggle();
+};
